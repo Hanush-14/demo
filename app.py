ca48ab145df1c65bbda6c48dc13b62e43bfdd6c4
Hanush-14 <hanushhanu1014@gmail.com>
2026-01-28 12:13:31 +0530

    Refactor Flask app and update route response

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
-@app.route('/')
-def Home():
-	return "Hello this is flask Application")
-if(__name__ =='__main__'):
-	app.run('host=0.0.0.0',5000)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Flask + Jenkins + Docker!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
